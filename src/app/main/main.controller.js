77017cc10516cc7e0dd7d3082ae590237dc596a8
Daniel Yim <danielyim@gmail.com>
2019-06-17 00:43:33 -0700

    Cleanup.

--- src/app/main/main.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('nubsOrg')
-  .controller('MainCtrl', function ($scope, AnchorSmoothScrollSvc) {
-
-    $scope.gotoElement = function (e) {
-      AnchorSmoothScrollSvc.scrollTo(e);
-    };
-  });
